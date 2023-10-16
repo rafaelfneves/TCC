8e6f85a87b06012215f763270c5e65ea9354a9a4
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2023-10-16 17:55:13 -0300

    update1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import hashlib # para criar o codigo do voucher 
@@ -24,7 +24,48 @@ class Todo(db.Model):
 # ====================== [MAIN ROUTE] ======================
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        # Tentará criar uma nova tarefa a partir dessa entrada, e tentar-a enviá-la ao nosso banco de dados
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Houve um problema adicionando sua tarefa'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    
+    except:
+         return 'Houve um problema deletando sua tarefa'
+     
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Houve um poblema atualizando sua tarefa'
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
