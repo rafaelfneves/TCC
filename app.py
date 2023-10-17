40d5e37e7e29c5f41a74acaa3ea9b5937a0088a9
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2023-10-17 02:38:45 -0300

    postgresql-flask

--- app.py
+++ app.py
@@ -4,68 +4,126 @@ from datetime import datetime
 import hashlib # para criar o codigo do voucher 
 import re #para validar formato de e-mail
 
+# ====================== [CONEXAO] ======================
 
 app = Flask(__name__)
-# Configuração do banco de dados PostgreSQL (substitua com suas próprias configurações)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db_config = {
+    'dbname': 'green_line',
+    'user': 'postgres',
+    'password': '123456',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+# Crie a URL de conexão com o PostgreSQL
+db_url = f'postgresql://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}:{db_config["port"]}/{db_config["dbname"]}'
+
+# Configure o SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# Teste a conexão
+if db.engine.connect():
+    print("Conexão bem-sucedida!")
+else:
+    print("Falha na conexão.")
     
-    
-    #Retornará apenas a tarefa e a ideia dessa tarefa que acabou de ser criada.
-    def __repr__(self):
-        return '<Task %r>' % self.id
+# ====================== [TABELAS] ======================
+class Catadores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    sobrenome = db.Column(db.String(100))
+    data_nascimento = db.Column(db.Date)
+    endereco = db.Column(db.String(255))
+    cidade = db.Column(db.String(100))
+    estado = db.Column(db.String(50))
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    experiencia_anos = db.Column(db.Integer)
+    area_atuacao = db.Column(db.String(100))
+    capacidade_carga_kg = db.Column(db.Numeric(10, 2))
 
-# ====================== [MAIN ROUTE] ======================
-@app.route('/', methods=['POST','GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-        
-        # Tentará criar uma nova tarefa a partir dessa entrada, e tentar-a enviá-la ao nosso banco de dados
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Houve um problema adicionando sua tarefa'
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-    
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    
-    try:
-        db.session.delete(task_to_delete)
+class Conquistas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    requerimento = db.Column(db.String(255), nullable=False)
+    pontos = db.Column(db.Integer, nullable=False)
+
+class Empresas(db.Model):
+    cnpj = db.Column(db.String(14), primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    endereco = db.Column(db.String(255))
+    email = db.Column(db.String(100))
+    telefone = db.Column(db.String(20))
+
+class Materiais(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text)
+    categoria = db.Column(db.String(50))
+    peso_medio_gramas = db.Column(db.Integer)
+    valor_venda = db.Column(db.Numeric(10, 2))
+
+class Veiculos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    marca = db.Column(db.String(100))
+    modelo = db.Column(db.String(100))
+    ano = db.Column(db.Integer)
+    placa = db.Column(db.String(10))
+    fk_catadores = db.Column(db.Integer, db.ForeignKey('catadores.id'))
+    catador = db.relationship('Catadores', back_populates='veiculos')
+
+class Vouchers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fk_conquistas = db.Column(db.Integer, db.ForeignKey('conquistas.id'))
+    fk_empresas = db.Column(db.String(14), db.ForeignKey('empresas.cnpj'))
+    fk_catadores = db.Column(db.Integer, db.ForeignKey('catadores.id'))
+    codigo = db.Column(db.String(256), nullable=False)
+
+# ====================== [ROTAS] ======================
+# =-=-= [Rotas CRUD para Catadores] =-=-=
+@app.route('/catadores', methods=['POST'])
+def create_catador():
+    data = request.json
+    new_catador = Catadores(**data)
+    db.session.add(new_catador)
+    db.session.commit()
+    return jsonify({'message': 'Catador criado com sucesso'}), 201
+
+@app.route('/catadores', methods=['GET'])
+def get_catadores():
+    catadores = Catadores.query.all()
+    result = [catador.__dict__ for catador in catadores]
+    return jsonify(result)
+
+@app.route('/catadores/<int:id>', methods=['GET'])
+def get_catador(id):
+    catador = Catadores.query.get(id)
+    if catador:
+        return jsonify(catador.__dict__)
+    return jsonify({'message': 'Catador não encontrado'}), 404
+
+@app.route('/catadores/<int:id>', methods=['PUT'])
+def update_catador(id):
+    data = request.json
+    catador = Catadores.query.get(id)
+    if catador:
+        for key, value in data.items():
+            setattr(catador, key, value)
         db.session.commit()
-        return redirect('/')
-    
-    except:
-         return 'Houve um problema deletando sua tarefa'
-     
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    
-    if request.method == 'POST':
-        task.content = request.form['content']
-        
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Houve um poblema atualizando sua tarefa'
-    else:
-        return render_template('update.html', task=task)
+        return jsonify({'message': 'Catador atualizado com sucesso'})
+    return jsonify({'message': 'Catador não encontrado'}), 404
 
+@app.route('/catadores/<int:id>', methods=['DELETE'])
+def delete_catador(id):
+    catador = Catadores.query.get(id)
+    if catador:
+        db.session.delete(catador)
+        db.session.commit()
+        return jsonify({'message': 'Catador excluído com sucesso'})
+    return jsonify({'message': 'Catador não encontrado'}), 404
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
